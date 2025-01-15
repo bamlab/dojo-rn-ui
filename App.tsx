b317c481d83aa11dcf4e452e7aa0f07bc02e7a19
uffoltzl <l.ufz@hotmail.fr>
2025-01-15 21:00:51 +0100

    add skia

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import "expo-dev-client";
 import { StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { MyFirstGestureHandler } from "./src/MyFirstGestureHandler/MyFirstGestureHandler";
+import { MyFirstSkia } from "./src/MyFirstSkia/MyFirstSkia";
 
 export default function App() {
   return (
     <GestureHandlerRootView>
       <View style={styles.container}>
-        <MyFirstGestureHandler />
+        <MyFirstSkia />
       </View>
     </GestureHandlerRootView>
   );
