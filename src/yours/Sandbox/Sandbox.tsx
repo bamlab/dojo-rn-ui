62ffcea23e21d29501a7c9177efdf7ef952e89fe
Julien IZZILLO <IZZILLJ1@colas.com>
2025-04-01 17:50:12 +0200

    add step 1 with bezier timing

--- src/yours/Sandbox/Sandbox.tsx
+++ src/yours/Sandbox/Sandbox.tsx
@@ -1,4 +1,4 @@
-import { CircularMenu } from "../../joconde/CircularPopupMenu/Step1";
+import { CircularMenu } from "../../joconde/CircularPopupMenu/Step1Bezier";
 
 export const Sandbox = () => {
   return <CircularMenu />;
