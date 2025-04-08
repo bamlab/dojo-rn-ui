eee12d209fc6387596e609139395cbf08fafa81b
Rémi Leroy <remil@bam.tech>
2025-04-08 12:08:19 +0200

    add step 3

--- src/yours/Sandbox/Sandbox.tsx
+++ src/yours/Sandbox/Sandbox.tsx
@@ -1,4 +1,4 @@
-import { CircularMenu } from "../../joconde/CircularPopupMenu/Step2";
+import { CircularMenu } from "../../joconde/CircularPopupMenu/Step3";
 
 export const Sandbox = () => {
   return <CircularMenu />;
